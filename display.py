f1fa8bf0a261fdafe8be06dcda3a14c514241c95
Anthony Perez <apere737@fiu.edu>
2017-11-17 14:43:46 +0000

    test

--- display.py
+++ display.py
@@ -1,3 +1,4 @@
+import time
 import threading
 from rgbmatrix import graphics
 from rgbmatrix import RGBMatrix
@@ -63,3 +64,4 @@ class Display(threading.Thread):
 
 if __name__ == '__main__':
     displaytext = Display()
+    displaytext.run()
